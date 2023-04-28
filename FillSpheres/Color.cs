7850d90630683388e936012274c1f63bbdf05679
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-04-28 01:41:11 +0100

    completed exercise 6

--- FillSpheres/Color.cs
+++ FillSpheres/Color.cs
@@ -6,55 +6,73 @@ using System.Threading.Tasks;
 namespace FillSpheres
 {
     public class Color
-{
-    private int red;
-    private int green;
-    private int blue;
-    private int alpha;
-
-    public Color(int red, int green, int blue, int alpha)
     {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
-        this.alpha = alpha;
-    }
+        private int red;
+        private int green;
+        private int blue;
+        private int alpha;
 
-    public Color(int red, int green, int blue)
-    {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
-        this.alpha = 255;
-    }
+        public Color(int red, int green, int blue, int alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
 
-    public int Red
-    {
-        get { return red; }
-        set { red = value; }
-    }
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = 255;
+        }
 
-    public int Green
-    {
-        get { return green; }
-        set { green = value; }
-    }
+        public int Red
+        {
+            get { return red; }
+            set { red = value; }
+        }
 
-    public int Blue
-    {
-        get { return blue; }
-        set { blue = value; }
-    }
+        public int Green
+        {
+            get { return green; }
+            set { green = value; }
+        }
 
-    public int Alpha
-    {
-        get { return alpha; }
-        set { alpha = value; }
-    }
+        public int Blue
+        {
+            get { return blue; }
+            set { blue = value; }
+        }
 
-    public int GetGrey()
-    {
-        return (red + green + blue) / 3;
+        public int Alpha
+        {
+            get { return alpha; }
+            set { alpha = value; }
+        }
+
+        public int GetGrey()
+        {
+            return (red + green + blue) / 3;
+        }
+
+        public string Name
+        {
+            get
+            {
+                switch (Red)
+                {
+                    case 255 when Green == 0 && Blue == 0:
+                        return "Red 100%";
+                    case 0 when Green == 255 && Blue == 0:
+                        return "Green 100%";
+                    case 0 when Green == 0 && Blue == 255:
+                        return "Blue 100%";
+                    default:
+                        return "Mixed";
+                }
+            }
+        }
     }
-}
 }
--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -11,9 +11,15 @@ namespace FillSpheres
             Sphere sphere3 = new Sphere(new Color(0, 0, 255), 420);
 
             Console.WriteLine("Your Spheres:");
-            Console.WriteLine("Sphere 1: color = [{0}, {1}, {2}], radius = {3}", sphere1.Color.Red, sphere1.Color.Green, sphere1.Color.Blue, sphere1.Radius);
-            Console.WriteLine("Sphere 2: color = [{0}, {1}, {2}], radius = {3}", sphere2.Color.Red, sphere2.Color.Green, sphere2.Color.Blue, sphere2.Radius);
-            Console.WriteLine("Sphere 3: color = [{0}, {1}, {2}], radius = {3}", sphere3.Color.Red, sphere3.Color.Green, sphere3.Color.Blue, sphere3.Radius);
+            Console.WriteLine("Sphere 1: color = [{0}, {1}, {2}], radius = {3}", 
+            sphere1.Color.Red, sphere1.Color.Green, sphere1.Color.Blue, 
+            sphere1.Radius);
+            Console.WriteLine("Sphere 2: color = [{0}, {1}, {2}], radius = {3}", 
+            sphere2.Color.Red, sphere2.Color.Green, sphere2.Color.Blue, 
+            sphere2.Radius);
+            Console.WriteLine("Sphere 3: color = [{0}, {1}, {2}], radius = {3}", 
+            sphere3.Color.Red, sphere3.Color.Green, sphere3.Color.Blue, 
+            sphere3.Radius);
 
             // Toss spheres back and forth
             const int numTosses = 5;
@@ -32,9 +38,18 @@ namespace FillSpheres
 
                 sphere3.Throw();
                 Console.WriteLine("After toss number {0}:", i + 1);
-                Console.WriteLine("Sphere 1: color = [{0}, {1}, {2}], radius = {3}, times thrown = {4}", sphere1.Color.Red, sphere1.Color.Green, sphere1.Color.Blue, sphere1.Radius, sphere1.GetTimesThrown());
-                Console.WriteLine("Sphere 2: color = [{0}, {1}, {2}], radius = {3}, times thrown = {4}", sphere2.Color.Red, sphere2.Color.Green, sphere2.Color.Blue, sphere2.Radius, sphere2.GetTimesThrown());
-                Console.WriteLine("Sphere 3: color = [{0}, {1}, {2}], radius = {3}, times thrown = {4}", sphere3.Color.Red, sphere3.Color.Green, sphere3.Color.Blue, sphere3.Radius, sphere3.GetTimesThrown());
+                Console.WriteLine("Sphere 1: color = [{0}, {1}, {2}], radius = {3}, times thrown = {4}", 
+                sphere1.Color.Red, sphere1.Color.Green, sphere1.Color.Blue, 
+                sphere1.Radius, sphere1.GetTimesThrown());
+                Console.WriteLine("Sphere 2: color = [{0}, {1}, {2}], radius = {3}, times thrown = {4}", 
+                sphere2.Color.Red, sphere2.Color.Green, sphere2.Color.Blue, 
+                sphere2.Radius, sphere2.GetTimesThrown());
+                Console.WriteLine("Sphere 3: color = [{0}, {1}, {2}], radius = {3}, times thrown = {4}", 
+                sphere3.Color.Red, sphere3.Color.Green, sphere3.Color.Blue, 
+                sphere3.Radius, sphere3.GetTimesThrown());
+                Console.WriteLine("Color name: {0}", new Color(255, 0, 0).Name);
+                Console.WriteLine("Color name: {0}", new Color(0, 255, 0).Name);
+                Console.WriteLine("Color name: {0}", new Color(0, 0, 255).Name);
             }
         }
     }
