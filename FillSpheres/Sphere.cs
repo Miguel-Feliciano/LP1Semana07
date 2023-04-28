2832ab73424f0990eaffa4da2523b2068983d213
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-04-28 01:12:57 +0100

    Completed exercise 3

---
+++ FillSpheres/Sphere.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FillSpheres
+{
+    public class Sphere
+    {
+        private Color color;
+        private int radius;
+        private int timesThrown;
+
+        public Sphere(Color color, int radius)
+        {
+            this.color = color;
+            this.radius = radius;
+            this.timesThrown = 0;
+        }
+
+        public void Pop()
+        {
+            radius = 0;
+        }
+
+        public void Throw()
+        {
+            if (radius > 0)
+            {
+                timesThrown++;
+            }
+        }
+
+        public int GetTimesThrown()
+        {
+            return timesThrown;
+        }
+
+        public Color Color
+        {
+            get { return color; }
+        }
+
+        public int Radius
+        {
+            get { return radius; }
+        }
+    }
+}
